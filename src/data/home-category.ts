42c8787753e1347365870afbc273f5e2a3b6596c
AhmedSaddique <ahmed.saddique12@gmail.com>
2026-08-25 18:20:40 +0500

    changes

--- src/data/home-category.ts
+++ src/data/home-category.ts
@@ -10,7 +10,7 @@ export const getSubcategoryOrder = (name: string) => {
   if (lower.includes('richmond spc eco')) return 3;
   if (lower.includes('richmond spc')) return 4;
 
-  // Specific Richmond LVT types
+
   if (lower.includes('richmond lvt comfort')) return 5;
   if (lower.includes('richmond lvt luxury')) return 5;
   if (lower.includes('richmond lvt')) return 6;
